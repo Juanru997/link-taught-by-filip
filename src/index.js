7a81647553a19b6c9770f1e2eeaaa5a311844d6f
Juanru <yz5026@nyu.edu>
2019-11-14 12:32:19 +0800

    with button and link!

--- src/index.js
+++ src/index.js
@@ -2,9 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Nihao from './Whatever'
+import {Route, BrowserRouter as Router} from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+const links = (
+    <Router>
+        <Route exact path='/' component={Nihao}/>
+        <Route exact path='/ha' component={App}/>
+    </Router>
+)
+ReactDOM.render(links, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
