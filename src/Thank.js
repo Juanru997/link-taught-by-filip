f697f9a0ad0f8e2c0595a3e49e13e01d3b54c961
蔡宇阳 <caiyuyang@caiyuyangdeMacBook-Air.local>
2019-12-05 11:31:08 +0800

    thank css

--- src/Thank.js
+++ src/Thank.js
@@ -1,15 +1,15 @@
 import React,{Component} from 'react';
 import {Route, Link, BrowserRouter as Router} from 'react-router-dom'
 import './App.css';
-
+import './Thank.css';
 
 function Thank() {
   return (
     <div className="Thank">
    
-        <p>
+        <div className="Font">
           Thank you for your feedback!
-        </p>
+        </div>
 
     </div>
   );
